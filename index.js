814829dc6ab7ac20306b586314d7ae8df6f6a1b2
Karanveer <karanveer43f@gmail.com>
2023-03-09 18:36:54 +0530

    set up the view engine

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const port = 8000;
 //using app.use middleware to route all paths coming to root to go to routes
 app.use("/", require("./routes"));
 
+//setting up the views engine
+
+app.set('view engine' , 'ejs');
+app.set('views' , './views');
+
 app.listen(port, function (err) {
   if (err) {
     console.log(`Error in running the server: ${err}`);

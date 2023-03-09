0644fff258d8a1fc602285e5f6b33fab8d5262ad
Karanveer <karanveer43f@gmail.com>
2023-03-09 17:31:40 +0530

    added a controller and a router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,7 @@
+module.exports.home = function(req ,res){
+    
+    return res.end('<h1>Controller is Accessed via Home Page</h1>')
+}
+
+
+//module.exports.actionName = function(req, res){};
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+//using app.get middleware to route all paths coming to root to go to routes
+app.get("/", require("./routes"));
+
 app.listen(port, function (err) {
   if (err) {
     console.log(`Error in running the server: ${err}`);

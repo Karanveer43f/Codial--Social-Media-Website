e702c947b516aa65a4fbade3609f2206db15260b
Karanveer <karanveer43f@gmail.com>
2023-03-27 19:51:21 +0530

    posts now render to home page along with user name

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,30 @@
+const Posts = require("../models/post");
+
 module.exports.home = function (req, res) {
-  console.log(req.cookies);
-  res.cookie('user_id',25)
-  return res.render("home", {
-    title: "Home",
-  });
+  // Posts.find({}, function (err, posts) {
+  //   if (err) {console.log("Error in fetching the posts");return;}
+
+  //   return res.render("home", {
+  //     title: "Home",
+  //     posts: posts,
+  //   });
+  // });
+
+  // If we want to show the user's name along side the post , we have to populate the user first
+
+  Posts.find({})
+    .populate("user")
+    .exec(function (err, posts) {
+      if (err) {
+        console.log("Error in fetching the posts");
+        return;
+      }
+
+      return res.render("home", {
+        title: "Home",
+        posts: posts,
+      });
+    });
 };
 
 //module.exports.actionName = function(req, res){};
